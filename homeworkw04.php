1476eef5b0de49b38236ddaa45f973cb71d4bc9b
Haochun Hsu <64048376+haochun6688@users.noreply.github.com>
2021-08-04 09:27:04 -0700

    Add files via upload
    homeworkW04 file is edited and updated

--- homeworkw04.php
+++ homeworkw04.php
@@ -1,27 +1,27 @@
 <?php
 $songlist= array (
     array( 
-    'name'      => 'Woody Allen',
-    'img'    =>  'https://yt3.ggpht.com/ytc/AKedOLQnremNjRoewRLYEqESCX7BojrLYqe3F_d3SHVPhg=s900-c-k-c0x00ffffff-no-rj',
-    'url' => 'https://en.wikipedia.org/wiki/Woody_Allen',
-    ),
+        'name'  => 'Woody Allen',
+        'img'   =>  'https://yt3.ggpht.com/ytc/AKedOLQnremNjRoewRLYEqESCX7BojrLYqe3F_d3SHVPhg=s900-c-k-c0x00ffffff-no-rj',
+        'url'   => 'https://en.wikipedia.org/wiki/Woody_Allen',
+        ),
     array( 
-        'name'      => 'Saint Peter',
-        'img'    =>  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Pope-peter_pprubens.jpg/220px-Pope-peter_pprubens.jpg',
-        'url' => 'https://en.wikipedia.org/wiki/Saint_Peter',
+        'name'  => 'Saint Peter',
+        'img'   =>  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Pope-peter_pprubens.jpg/220px-Pope-peter_pprubens.jpg',
+        'url'   => 'https://en.wikipedia.org/wiki/Saint_Peter',
         ),
 
-        array( 
-            'name'      => 'David',
-            'img'    =>  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/David_SM_Maggiore.jpg/220px-David_SM_Maggiore.jpg',
-            'url' => 'https://en.wikipedia.org/wiki/David',
-            ),
+    array( 
+        'name'  => 'David',
+        'img'   =>  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/David_SM_Maggiore.jpg/220px-David_SM_Maggiore.jpg',
+        'url'   => 'https://en.wikipedia.org/wiki/David',
+        ),
 
 
-);
-foreach ($songlist as $key => $value) {
+                );
+foreach ($songlist as $list) {
     # code...
-    echo "<p>Singer Information: {$value['url']}</p>";
+    echo "<p>Singer Information: {$list['url']}</p>";
 }
 ?>
 <!DOCTYPE html>
@@ -34,6 +34,9 @@ foreach ($songlist as $key => $value) {
 </head>
 <body>
     <h1>Homework_for_week04!</h1>
-    
+    <?php foreach ($songlist as $list);?>
+    <li>
+        <a href="<?php echo $list['url'];?>" target="_blank"><img src="<?php echo $list['img'];?>" alt=""></a>
+    </li>
 </body>
 </html>
