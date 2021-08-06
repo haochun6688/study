0547272973dea3db602e892b8c21e1a4fa2c4587
Haochun Hsu <64048376+haochun6688@users.noreply.github.com>
2021-08-05 18:54:34 -0700

    Add files via upload

--- homeworkw04.php
+++ homeworkw04.php
@@ -19,10 +19,27 @@ $songlist= array (
 
 
                 );
+$booklist =array("josh","peter",true,false,999,"david");
+print_r($booklist);
+$newbooklist = join("  ----",$booklist);
+echo $newbooklist;
+
 foreach ($songlist as $list) {
     # code...
     echo "<p>Singer Information: {$list['url']}</p>";
 }
+//array_push practice 8/5/2021
+$test = array();
+    for ($i=10; $i <=100 ; $i+=10) 
+    { 
+        # code...
+        if ($i !=70 && $i !=90) 
+        {
+            array_push($test,$i);
+        }
+    }                   
+echo join(", ",$test);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,12 +48,20 @@ foreach ($songlist as $list) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>homeworkw04</title>
+    <style>
+        .otherlist img{
+            width:80px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Homework_for_week04!</h1>
-    <?php foreach ($songlist as $list);?>
+    <div class="otherlist">
+    <h2>Homework_for_week04!</h2>
+    <?php foreach ($songlist as $list):?>
     <li>
         <a href="<?php echo $list['url'];?>" target="_blank"><img src="<?php echo $list['img'];?>" alt=""></a>
     </li>
+    <?php endforeach ?>
+    </div>
 </body>
 </html>
